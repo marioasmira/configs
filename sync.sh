2d0be6ca1d9585fa6d2a993d6eac85e5dee45d06
Mário S. Mira <marioasmira@gmail.com>
2025-03-28 13:21:53 +0100

    Added options to the bash file to select which files to back up or refresh

--- sync.sh
+++ sync.sh
@@ -2,9 +2,31 @@
 
 if [[ $1 == "backup" ]];
 then
-	rsync -ur ~/.config/nvim/ nvim
+    case $2 in
+
+	nvim)
+	    rsync -ur ~/.config/nvim/ nvim
+	    ;;
+	bash)
+	    rsync -ur ~/.bash_aliases .
+	    ;;
+	*)
+	    echo -n "The second argument needs to specify the configuration files to back up."
+	    ;;
+    esac
 elif [[ $1 == "refresh" ]]; then
-	rsync -ur nvim/ ~/.config/nvim
+    case $2 in
+
+	nvim)
+	    rsync -ur nvim/ ~/.config/nvim
+	    ;;
+	bash)
+	    rsync -ur .bash_aliases ~
+	    ;;
+	*)
+	    echo -n "The second argument needs to specify the configuration files to refresh."
+	    ;;
+    esac
 else
 	echo "The first argument should be either backup to backup local files or refresh to bring in repository files."
 fi
